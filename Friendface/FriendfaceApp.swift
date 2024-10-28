6741063c0381e5b5dc97f49bb7fe69486a0159c1
Anna Filin <68apple@gmail.com>
2024-10-28 14:39:07 +0200

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Anna Filin on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
